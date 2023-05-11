b1f4a800c6a4594100d0e5e936b779e3074bb779
GiedriusMauza <giedrius.mauza@gmail.com>
2023-05-11 11:10:21 +0300

    library models added

---
+++ src/test/java/lt/viko/eif/gmauza/librarymanagementrest/models/Borrowing.java
@@ -0,0 +1,108 @@
+package lt.viko.eif.gmauza.librarymanagementrest.models;
+
+
+
+public class Borrowing {
+
+    protected String borrowingId;
+    protected String borrowDate;
+    protected String returnDate;
+    protected Item item;
+
+    /**
+     * Gets the value of the borrowingId property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getBorrowingId() {
+        return borrowingId;
+    }
+
+    /**
+     * Sets the value of the borrowingId property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setBorrowingId(String value) {
+        this.borrowingId = value;
+    }
+
+    /**
+     * Gets the value of the borrowDate property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getBorrowDate() {
+        return borrowDate;
+    }
+
+    /**
+     * Sets the value of the borrowDate property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setBorrowDate(String value) {
+        this.borrowDate = value;
+    }
+
+    /**
+     * Gets the value of the returnDate property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    /**
+     * Sets the value of the returnDate property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setReturnDate(String value) {
+        this.returnDate = value;
+    }
+
+    /**
+     * Gets the value of the item property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Item }
+     *     
+     */
+    public Item getItem() {
+        return item;
+    }
+
+    /**
+     * Sets the value of the item property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Item }
+     *     
+     */
+    public void setItem(Item value) {
+        this.item = value;
+    }
+
+}
---
+++ src/test/java/lt/viko/eif/gmauza/librarymanagementrest/models/Subscribers.java
@@ -0,0 +1,39 @@
+package lt.viko.eif.gmauza.librarymanagementrest.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subscribers {
+
+    protected List<Subscriber> subscriber;
+
+    /**
+     * Gets the value of the subscriber property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the Jakarta XML Binding object.
+     * This is why there is not a <CODE>set</CODE> method for the subscriber property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getSubscriber().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Subscriber }
+     * 
+     * 
+     */
+    public List<Subscriber> getSubscriber() {
+        if (subscriber == null) {
+            subscriber = new ArrayList<Subscriber>();
+        }
+        return this.subscriber;
+    }
+
+}
