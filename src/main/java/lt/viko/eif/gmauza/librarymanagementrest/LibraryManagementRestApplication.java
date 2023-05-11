992e45fae71723a697ab1c5a4e0d8a9219497c91
GiedriusMauza <giedrius.mauza@gmail.com>
2023-05-11 10:31:15 +0300

    Initial commit

---
+++ src/main/java/lt/viko/eif/gmauza/librarymanagementrest/LibraryManagementRestApplication.java
@@ -0,0 +1,13 @@
+package lt.viko.eif.gmauza.librarymanagementrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementRestApplication.class, args);
+	}
+
+}
